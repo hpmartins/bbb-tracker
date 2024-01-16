7805e0f228244805e0810be69acff3aa5845fe70
hpmartins <hpmartins@gmail.com>
2024-01-16 06:27:21 +0000

    recent activity

--- index.ts
+++ index.ts
@@ -3,13 +3,21 @@ import express from 'express';
 import path from 'path';
 import { IParticipante, Paredao, Participante } from './db';
 
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+const TIMEZONE = 'America/Sao_Paulo';
+
 const countUnique = (arr: any[]) => {
-    const counts: {[key: string]: any} = {};
+    const counts: { [key: string]: any } = {};
     for (var i = 0; i < arr.length; i++) {
-       counts[arr[i].to] = 1 + (counts[arr[i].to] || 0);
-    };
+        counts[arr[i].to] = 1 + (counts[arr[i].to] || 0);
+    }
     return counts;
- };
+};
 
 const getParticipants = async () => {
     return await Participante.aggregate<IParticipante>()
@@ -34,18 +42,88 @@ const getParticipants = async () => {
 };
 
 const getParedoes = async () => {
-    const paredoes = await Paredao.find()
+    const paredoes = await Paredao.find();
     const votos = paredoes.map((paredao) => {
-        const normais = paredao.votos.filter(x => !x.extra)
-        const indicacao = paredao.votos.filter(x => x.extra?.indicacao)[0].to
+        const normais = paredao.votos.filter((x) => !x.extra);
+        const indicacao = paredao.votos.filter((x) => x.extra?.indicacao)[0].to;
         return {
             _id: paredao._id,
             normais: countUnique(normais),
-            indicacao: indicacao,
+            indicacao: indicacao
+        };
+    });
+    return { paredoes, votos };
+};
+
+const getActivityDeltas = async () => {
+    // const activity = await Participante.aggregate()
+    //     .project({
+    //         nome: '$_id.nome',
+    //         modified: '$_id.modified',
+    //         estalecas: 1
+    //     })
+    //     .project({
+    //         _id: 0
+    //     })
+    //     .sort({ modified: -1 });
+
+    const grouped: {
+        _id: string;
+        list: { _id: Date; nomePopular: string; imagem: string; estalecas: number }[];
+    }[] = await Participante.aggregate()
+        .sort({ '_id.modified': -1 })
+        .group({
+            _id: '$_id.nome',
+            list: {
+                $push: {
+                    _id: '$_id.modified',
+                    nomePopular: '$nomePopular',
+                    imagem: '$imagem',
+                    estalecas: '$estalecas'
+                }
+            }
+        })
+        .addFields({
+            list: {
+                $sortArray: {
+                    input: '$list',
+                    sortBy: { modified: -1 }
+                }
+            },
+            n: { $size: '$list' }
+        })
+        .match({
+            n: { $gte: 2 }
+        })
+        .project({
+            n: 0
+        });
+
+    const deltas: {
+        nome: string;
+        nomePopular: string;
+        imagem: string;
+        modified: Date;
+        delta: number;
+    }[] = [];
+
+    grouped.map((x) => {
+        for (let i = 0; i < x.list.length - 1; i++) {
+            let diff = x.list[i].estalecas - x.list[i + 1].estalecas;
+            if (diff != 0) {
+                deltas.push({
+                    nome: x._id,
+                    nomePopular: x.list[i].nomePopular,
+                    imagem: x.list[i].imagem,
+                    modified: x.list[i]._id,
+                    delta: x.list[i].estalecas - x.list[i + 1].estalecas
+                });
+            }
         }
-    })
-    return { paredoes, votos }
-}
+    });
+
+    return deltas.sort((a, b) => (dayjs(a.modified).isAfter(b.modified) ? -1 : 1));
+};
 
 const run = async () => {
     const app = express();
@@ -71,6 +149,8 @@ const run = async () => {
         const vip = participants.filter((x) => !x.lider && x.grupo === 'VIP');
         const xepa = participants.filter((x) => !x.lider && x.grupo === 'XEPA');
 
+        const deltas = await getActivityDeltas();
+
         res.render('index', {
             all_participants: all_participants,
             participants: participants,
@@ -79,6 +159,10 @@ const run = async () => {
             xepa: xepa,
             paredoes: paredoes,
             votos: votos,
+            activity: deltas.map((x) => ({
+                ...x,
+                modified: dayjs(x.modified).tz(TIMEZONE).format('DD/MM HH:mm')
+            }))
         });
     });
 
