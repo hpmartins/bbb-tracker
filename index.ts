ce7cd5eb46580b5549a7342597efe6acd2f1ecc8
hpmartins <hpmartins@gmail.com>
2024-01-19 22:13:58 +0000

    mapa de cores pros votos

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import path from 'path';
 import { IParticipante, Paredao, Participante } from './db';
+import chroma from 'chroma-js';
 
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
@@ -128,17 +129,6 @@ const getVotos = async () => {
         .sort({ paredao: 1 });
 
     return votos;
-    // const votos = paredoes.map((paredao) => {
-    //     const normais = paredao.votos.filter((x) => !x.extra);
-    //     const indicacao = paredao.votos.filter((x) => x.extra?.indicacao)[0].to;
-    //     return {
-    //         _id: paredao._id,
-    //         normais: normais,
-    //         normais_unique: countUnique(normais),
-    //         indicacao: indicacao
-    //     };
-    // });
-    // return { number: paredoes.length, votos: votos };
 };
 
 const run = async () => {
@@ -173,6 +163,33 @@ const run = async () => {
 
         const deltas = await getActivityDeltas();
 
+        let votos_count: number[] = [];
+        for (let p1 of all_participants) {
+            for (let p2 of all_participants) {
+                votos_count.push(
+                    votos.filter((x) => x.from == p1._id.nome && x.to == p2._id.nome).length
+                );
+            }
+        }
+        votos_count = [...new Set(votos_count.sort((a, b) => a - b))];
+
+        console.log(votos_count)
+
+        const votos_cores = Object.fromEntries(
+            votos_count.map((x) => {
+                let bg_color = chroma
+                    .scale('Spectral')
+                    .domain([votos_count[votos_count.length - 1], votos_count[0]])(x);
+
+                let fg_color =
+                    chroma.contrast(bg_color, chroma('white')) > 7
+                        ? chroma('white')
+                        : chroma('black');
+
+                return [x, { bg: bg_color.hex(), fg: fg_color.hex() }];
+            })
+        );
+
         res.render('index', {
             all_participants: all_participants,
             participants: participants,
@@ -181,6 +198,7 @@ const run = async () => {
             xepa: xepa,
             paredoes: paredoes,
             votos: votos,
+            votos_cores: votos_cores,
             activity: deltas.map((x) => ({
                 ...x,
                 modified: dayjs(x.modified).tz(TIMEZONE).format('DD/MM HH:mm')
